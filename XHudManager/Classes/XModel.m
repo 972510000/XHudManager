4764ea3bb91a7b1a789c5c4a91f1334dc92e3ae0
Xu <972510000@qq.com>
2020-04-23 12:25:29 +0800

    [create]public pod file

---
+++ XHudManager/Classes/XModel.m
@@ -0,0 +1,13 @@
+//
+//  XModel.m
+//  XHudManager_Example
+//
+//  Created by WenQiangXu on 2020/4/23.
+//  Copyright © 2020 972510000@qq.com. All rights reserved.
+//
+
+#import "XModel.h"
+
+@implementation XModel
+
+@end
